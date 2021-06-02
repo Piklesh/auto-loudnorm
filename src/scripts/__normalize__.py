5ade8ce6e26e3d3c4e335638e8fd3cbe6700acd2
Multi8000 <marcos.martins0@outlook.com>
2021-06-02 00:35:51 -0300

    feat: parse ffmpeg output as a valid json using regex

--- src/scripts/__normalize__.py
+++ src/scripts/__normalize__.py
@@ -1,3 +1,20 @@
 from pathlib import Path
-import subprocess
+from subprocess import check_output, STDOUT
+from __validate__ import is_audio_file, GraceffulyGetDictKey
+from json import loads
+from re import search
 import librosa
+
+
+REGEX_EXPRESSION = r'\{(\r.*|\n.*)+[}$]'
+gracefully = GraceffulyGetDictKey()
+
+
+ffmpeg_command = '''ffmpeg -hide_banner -i misc/audio_file_2.wav -af loudnorm=I=-16:dual_mono=true:TP=-1.5:LRA=11:print_format=json -f null -'''
+output = check_output(ffmpeg_command, stderr = STDOUT).decode('utf-8')
+
+
+match = search(pattern = REGEX_EXPRESSION, string = output).group(0)
+json_output = loads(match)
+
+print(gracefully.format('{input_i}', **json_output))
