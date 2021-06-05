d7dd0d71c7a1697aeaf63427e3be184ddf6772fc
Multi8000 <marcos.martins0@outlook.com>
2021-06-05 00:56:55 -0300

    feat: create __utils__ with 'fill_audio_length' and 'generate_txt' functions

---
+++ src/utils/__utils__.py
@@ -0,0 +1,12 @@
+import os
+
+
+def generate_txt(file):
+    ...
+
+
+def fill_audio_lenght(file):
+    # ffmpeg -f concat -safe 0 -i files.txt -c copy output.wav
+    # https://trac.ffmpeg.org/wiki/Concatenate
+    # return PATH of the filled file, e.g 'misc/new_file.wav'
+    ...
