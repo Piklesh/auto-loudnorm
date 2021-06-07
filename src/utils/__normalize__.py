c6f914fc25ac18059587695316b05a7cb2e3bcef
Marcos Martins <marcos.martins0@outlook.com>
2021-06-06 21:55:09 -0300

    feat: improve 'second_pass' function and all function returns

--- src/utils/__normalize__.py
+++ src/utils/__normalize__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from subprocess import (check_output, STDOUT)
 from __validate__ import (is_audio_file, has_length_gte_3s)
-from __utils__ import file_size
+from __utils__ import (file_size, make_directory)
 from json import loads
 from re import search
 
@@ -24,25 +24,28 @@ def first_pass(file, target_lufs):
         ffmpeg_output = check_output(ffmpeg_command, stderr = STDOUT, shell = True).decode('utf-8')
 
         capture_metrics = search(pattern = REGEX_EXPRESSION, string = ffmpeg_output).group(0)
-        capture_metrics = loads(capture_metrics)
-        capture_metrics['file_size'] = file_size(file)
+        metrics = loads(capture_metrics)
+        metrics['file_size'] = file_size(file)
 
-        return {'metrics': capture_metrics, 'sucess': True}
+        return {'metrics': metrics, 'sucess': True}
 
-    return {'file': file, 'error': 'Its not a valid audio file.', 'sucess': False}
+    return {'file': file, 'error': 'Invalid audio file.', 'sucess': False}
 
 
-def second_pass(file, target_lufs, output_file):
+def second_pass(file, target_lufs, output_folder = 'misc/normalized'):
+    make_directory('misc/normalized')
+    file_name = Path(file).name
     result = first_pass(file, target_lufs)
 
     if result['sucess'] == False:
-        return {'file': file, 'error': 'Its not a valid audio file.', 'sucess': False}
+        return {'file': file, 'error': 'Invalid audio file.', 'sucess': False}
 
     metrics = result['metrics']
 
     # TO-DO: default values for measured_I, measured_TP, measured_LRA, measured_thresh
     # TO-DO: output_file -> output_folder, new file name will be the old file name
-    ffmpeg_command = f'''ffmpeg -hide_banner -nostdin -i {file} -af loudnorm=I={target_lufs}:TP=-1.5:LRA=11:measured_I={metrics['input_i']}:measured_TP={metrics['input_tp']}:measured_LRA={metrics['input_lra']}:measured_thresh={metrics['input_thresh']}:offset={metrics['target_offset']}:linear=true:print_format=summary -y {output_file}'''
+    # TO-DO: option to convert or not to .wav, arg to_wav=True
+    ffmpeg_command = f'''ffmpeg -hide_banner -nostdin -i {file} -af loudnorm=I={target_lufs}:TP=-1.5:LRA=11:measured_I={metrics['input_i']}:measured_TP={metrics['input_tp']}:measured_LRA={metrics['input_lra']}:measured_thresh={metrics['input_thresh']}:offset={metrics['target_offset']}:linear=true:print_format=summary -y {output_folder}/{file_name}'''
     ffmpeg_output = check_output(ffmpeg_command, stderr = STDOUT, shell = True).decode('utf-8')
 
     # TO-DO: IF new_size > old_size print warning file_size
