f49c0d0f43a3105abc4db68285d72a7bccbb071e
Marcos Martins <marcos.martins0@outlook.com>
2021-06-06 21:54:00 -0300

    feat: 'delete_file' function and improve function returns

--- src/utils/__utils__.py
+++ src/utils/__utils__.py
@@ -11,7 +11,7 @@ def file_size(file):
         return getsize(file)
 
     except FileNotFoundError:
-        return {'status_code': 0}
+        return {'file': file, 'error': 'File not found.', 'sucess': False}
 
 
 def make_directory(path):
@@ -22,21 +22,24 @@ def delete_directory(path):
     rmtree(Path(path), ignore_errors = True)
 
 
+def delete_file(path):
+    Path(path).unlink(missing_ok = True)
+
+
 def generate_txt(file):
     try:
         path = Path.cwd()
 
         audio_duration = get_duration(filename = file)
-        # 5 its no a magic number. EBU R128 recommends that audio files be at least 3 seconds duration.
-        # we use 5 for more precision.
-        n_to_duplicate = ceil(5 / audio_duration)
+        # EBU R128 recommends that audio files be at least 3 seconds duration.
+        n_to_duplicate = ceil(3 / audio_duration)
 
         for _ in range(n_to_duplicate):
             with open(f'files.txt', 'a') as f:
                 f.write(f'file {file}\n')
 
     except FileNotFoundError:
-        return {'sucess': False}
+        return {'file': file, 'error': 'File not found.', 'sucess': False}
 
 
 def fill_audio_length(file):
@@ -47,15 +50,11 @@ def fill_audio_length(file):
     ffmpeg_command = f'''ffmpeg -f concat -safe 0 -i "files.txt" -c copy -y "misc/temp/file_name_filled.ogg"'''
     ffmpeg_output = check_output(ffmpeg_command, stderr = STDOUT).decode('utf-8')
 
-    # TO-DO: delete files.txt after fill
+    delete_file('files.txt')
     #print(f'File {file} filled up to 5 seconds and saved at \'misc/temp/{file}_filled.wav\'')
-    print(0)
+    #print(0)
     #return 0
 
 
 def back_normal_length(file):
     ...
-
-
-#fill_audio_length('misc/audio_file_6.ogg')
-#generate_txt('misc/audio_file_6.ogg')
